08b95741f2be3f799e6292680536690fecac689e
Maksim.Lamanskiy <lammax2012@gmail.com>
2021-04-02 20:50:59 +0300

    Test for URL

--- SkyEngSampleTests/SkyEngSampleTests.swift
+++ SkyEngSampleTests/SkyEngSampleTests.swift
@@ -18,16 +18,11 @@ class SkyEngSampleTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testURL() throws {
+        let url = REST.URLS().getEndPoint(for: .search, text: "text")
+        XCTAssertTrue(url != nil)
+        XCTAssertTrue(url?.absoluteString == "https://dictionary.skyeng.ru/api/public/v1/words/search?search=text")
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 
 }
